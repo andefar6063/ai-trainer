9a4fed788511679e6a22a46a9f3aa280067a4f1c
andefar6063 <antonjimmyheino@gmail.com>
2024-04-30 10:25:50 +0200

    Changed the bot to an assistant and fixed some errors

--- client.py
+++ client.py
@@ -1,15 +1,14 @@
 import os
-from openai import OpenAI
+from typing_extensions import override
+from openai import OpenAI, AssistantEventHandler
 from utils import get_json_data, console_log
 
-class AIClient():
+class AIClient:
     def __init__(self):
-        self.client = OpenAI(
-            api_key=os.environ.get("OPENAI_API_KEY"),
-        )
+        self.client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
         self.json_data = get_json_data()
         self.trainer = None
-        self.question = None
+        self.thread = None
 
     def get_trainer(self, prompt):
         while True:
@@ -17,23 +16,75 @@ class AIClient():
             for key, value in self.json_data.items():
                 if user_input == key.lower() or user_input == value["command_key"].lower():
                     self.trainer = key
+                    print(self.trainer)
                     return
             console_log("Invalid input. Please try again.", "red")
 
-    def get_question(self, prompt):
-        self.question = input(prompt).lower()
+    def create_thread(self):
+        self.thread = self.client.beta.threads.create()
 
-    def client_completion(self):
+    def add_message_to_thread(self, content):
+        self.client.beta.threads.messages.create(
+            thread_id=self.thread.id,
+            role="user",
+            content=content,
+        )
+
+    def run_assistant(self):
+        class EventHandler(AssistantEventHandler):    
+            @override
+            def on_text_created(self, text) -> None:
+                print(f"\nassistant > ", end="", flush=True)
+                
+            @override
+            def on_text_delta(self, delta, snapshot):
+                print(delta.value, end="", flush=True)
+                
+            def on_tool_call_created(self, tool_call):
+                print(f"\nassistant > {tool_call.type}\n", flush=True)
+            
+            def on_tool_call_delta(self, delta, snapshot):
+                if delta.type == 'code_interpreter':
+                    if delta.code_interpreter.input:
+                        print(delta.code_interpreter.input, end="", flush=True)
+                if delta.code_interpreter.outputs:
+                    print(f"\n\noutput >", flush=True)
+                    for output in delta.code_interpreter.outputs:
+                        if output.type == "logs":
+                            print(f"\n{output.logs}", flush=True)
+
+        with self.client.beta.threads.runs.stream(
+                thread_id=self.thread.id,
+                assistant_id=self.json_data[self.trainer]["key"],
+                instructions="Please address the user as User.",
+                event_handler=EventHandler(),
+        ) as stream:
+            stream.until_done()
+
+    """
+    async def run_assistant(self):
         try:
-            completion = self.client.chat.completions.create(
-                model="gpt-4",
-                temperature=0,
-                max_tokens=100,
-                messages=[
-                    {"role": "system", "content": self.json_data[self.trainer]["content"]},
-                    {"role": "user", "content": self.question}
-                ]
+            run = self.client.beta.threads.runs.create_and_poll(
+                thread_id=self.thread.id,
+                assistant_id=self.json_data[self.trainer]["key"],
+                instructions="Please address the user as User."
             )
-            return completion.choices[0].message.content
-        except:
-            return "An error occurred while processing your request."
+        except Exception as e:
+            print(e)
+            return
+
+        while run.status != 'completed':
+            await asyncio.sleep(1)  # Adjust sleep time as needed
+            run.poll()  # Poll the run status
+
+        if run.status == 'completed': 
+            messages = self.client.beta.threads.messages.list(thread_id=self.thread.id)
+            for message in messages:
+                print(message.content)
+        else:
+            print(run.status)"""
+
+
+
+
+    
--- pages/question.py
+++ pages/question.py
@@ -1,21 +1,24 @@
 from utils import clear_screen, show_json_data, console_log
 from client import AIClient
         
-def question():
+async def question():
     console_log("Here are all the trainers you can use!\n")
     show_json_data()
 
     client = AIClient()
     client.get_trainer("Choose your trainer: ")
-    client.get_question("What is your question? ")
-    result = client.client_completion()
+    client.create_thread()
+    message_active = True
+    while message_active:
+        client.add_message_to_thread(input("Type here: "))
+        client.run_assistant()
+        print("\n")
+        if input("Would you like to close the thread: ['y' or 'n'] ") == "y":
+            message_active = False
+        else:
+            continue
+
     
-    console_log("\n")
-    if result != "An error occurred while processing your request.":
-        console_log(result, "blue")
-    else:
-        console_log(result, "red")
-    console_log("\n")
     user_return = input("Return to the main page: ['y' or 'n'] ").lower()
 
     if user_return == "y":
@@ -27,4 +30,3 @@ def question():
 
 if __name__ == "__main__":
     question()
-
